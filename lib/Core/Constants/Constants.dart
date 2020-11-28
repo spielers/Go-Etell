af840079a79c3b2de12ba26482ba8b2bcb972f4f
Suraj Patil <48085134+spielers@users.noreply.github.com>
2020-11-28 14:43:04 +0530

    a

---
+++ lib/Core/Constants/Constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  static const mapApiKey = "AIzaSyCRQdi-vULWok9FmeFNlKxSywacZMCYI4w";
+  static const anotherApiKey = "AIzaSyBv6SFQhOThOQ8TAI101fqkgWBONIyfeUc";
+  static const destinationMarkerId = "DestinationMarker";
+  static const pickupMarkerId = "PickupMarker";
+  static const currentLocationMarkerId = "currentLocationMarker";
+  static const currentRoutePolylineId = "currentRoutePolyline";
+  static const driverMarkerId = "CurrentDriverMarker";
+  static const driverOriginPolyId = "driverOriginPolyLine";
+}
---
+++ lib/Core/Constants/colorConstants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class ConstantColors {
+  static const Color PrimaryColor = Color(0xFF2B92EC);
+  static const Color ActivePink = Color(0xFF37FB78);
+  static const Color DeepBlue = Color(0xff13034d);
+}
